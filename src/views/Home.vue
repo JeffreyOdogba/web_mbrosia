0be9d784383288ab220ecc69cbaf75be5ad3d0ee
Jeffrey Odogba <odogbajeffrey@gmail.com>
2020-11-13 02:46:11 -0500

    rm: css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,9 @@ import BannerHomeMbrosia from "@/components/BannerHome_Mbrosia.vue";
 export default {
   name: "Home",
   components: {
-    BannerHomeMbrosia
-  }
+    BannerHomeMbrosia,
+  },
 };
 </script>
 
-<style scoped>
-.home {
-}
-</style>
+<style scoped></style>
